ac0828eb9f7b68532df0feb546e48a034ae2cfa6
Shadat Rahman <shadat.rahman.464@gmail.com>
2022-06-20 02:08:37 +0600

    profile screen done;

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:social_ui/widgets/custom_drawer.dart';
 
+import '../data/data.dart';
 import '../models/user_model.dart';
+import '../widgets/post_carousel.dart';
 import '../widgets/profile_clipper.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -13,12 +15,23 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  final GlobalKey<ScaffoldState> _scafoldState = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+  late PageController _yourPostsController;
+  late PageController _yourFavoritePostController;
+
+  @override
+  void initState() {
+    super.initState();
+    _yourPostsController =
+        PageController(initialPage: 0, viewportFraction: 0.8);
+    _yourFavoritePostController =
+        PageController(initialPage: 0, viewportFraction: 0.8);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scafoldState,
+      key: _scaffoldState,
       drawer: CustomDrawer(),
       body: SingleChildScrollView(
         child: Column(
@@ -31,7 +44,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   child: Image(
                     height: 300,
                     width: double.infinity,
-                    image: AssetImage(widget.user.backgroundImageUrl.toString()),
+                    image:
+                        AssetImage(widget.user.backgroundImageUrl.toString()),
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -39,7 +53,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   top: 50,
                   left: 20,
                   child: IconButton(
-                    onPressed: () => _scafoldState.currentState?.openDrawer(),
+                    onPressed: () => _scaffoldState.currentState?.openDrawer(),
                     icon: Icon(
                       Icons.menu,
                       size: 30,
@@ -54,7 +68,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       shape: BoxShape.circle,
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black45,
+                          color: Colors.black54,
                           offset: Offset(0, 2),
                         ),
                       ],
@@ -63,14 +77,77 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       child: Image(
                         height: 120,
                         width: 120,
-                        image: AssetImage(widget.user.profileImageUrl.toString()),
+                        image:
+                            AssetImage(widget.user.profileImageUrl.toString()),
                         fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
               ],
-            )
+            ),
+            Text(
+              widget.user.name.toString(),
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Following',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black45,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      widget.user.following.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Followers',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black54,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      widget.user.followers.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            PostCarousel(
+              pageController: _yourPostsController,
+              title: 'Your Posts',
+              posts: widget.user.posts!.toList(),
+            ),
+            PostCarousel(
+              pageController: _yourFavoritePostController,
+              title: 'Favorites',
+              posts: widget.user.favorites!.toList(),
+            ),
+            SizedBox(height: 50),
           ],
         ),
       ),
